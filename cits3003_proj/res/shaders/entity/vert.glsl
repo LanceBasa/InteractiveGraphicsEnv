8c7e7123ffbb538f5c80dde99972fa1f33089dae
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2024-04-26 16:17:31 +0800

    Fixed shader file, needed to just fix texture coordinates

--- cits3003_proj/res/shaders/entity/vert.glsl
+++ cits3003_proj/res/shaders/entity/vert.glsl
@@ -37,7 +37,7 @@ void main() {
     // Transform vertices
     vec3 ws_position = (model_matrix * vec4(vertex_position, 1.0f)).xyz;
     vec3 ws_normal = normalize(normal_matrix * normal);
-    vertex_out.texture_coordinate = texture_coordinate;
+    vertex_out.texture_coordinate = texture_coordinate * texture_scale;
 
     gl_Position = projection_view_matrix * vec4(ws_position, 1.0f);
 
