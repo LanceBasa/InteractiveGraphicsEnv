800b01e87bf33e7ee9d50ee4fe6e32bfc517fd9b
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2024-04-30 16:23:41 +0800

    added texture scale multiplication to fragment shader for lights.glsl in common

--- cits3003_proj/res/shaders/entity/frag.glsl
+++ cits3003_proj/res/shaders/entity/frag.glsl
@@ -46,7 +46,7 @@ void main() {
     );
 
         // Resolve the per vertex lighting with per fragment texture sampling.
-    vec3 resolved_lighting = resolve_textured_light_calculation(vertex_out_lighting_result, diffuse_texture, specular_map_texture, vertex_out_texture_coordinate );
+    vec3 resolved_lighting = resolve_textured_light_calculation(vertex_out_lighting_result, diffuse_texture, specular_map_texture, vertex_out_texture_coordinate * texture_scale); 
     out_colour = vec4(resolved_lighting, 1.0f);
     out_colour.rgb = pow(out_colour.rgb, vec3(inverse_gamma));
 }
